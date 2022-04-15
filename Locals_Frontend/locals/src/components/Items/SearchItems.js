9ecce5530f7e12bd3d21bd575f88ba8046890941
Paula Kurniawan <paula.kurn@gmail.com>
2022-04-15 11:22:55 +1000

    Buy order working

--- Locals_Frontend/locals/src/components/Items/SearchItems.js
+++ Locals_Frontend/locals/src/components/Items/SearchItems.js
@@ -2,33 +2,66 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import {useLocation} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
-import {useNavigate} from 'react-router-dom';
+import {useContext} from 'react';
+import config from '../../config.json'
+import AuthContext from '../../context/AuthContext';
 
+const axios = require('axios').default;
+
+const getToday = () => {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1; // Months start at 0!
+  let dd = today.getDate();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  const formattedDate = dd + '/' + mm + '/' + yyyy;
+  return formattedDate;
+}
 
 
 const SearchItems = () => {
+  const user = useContext(AuthContext);
   const {state} = useLocation();
   console.log(state)
-  const {items, query, suburbs} = state;
-  console.log(suburbs);
+  let {items, query, suburbs} = state;
+
+  items = items.map(x=>({...x, deliveryfee: parseFloat((Math.random()*3 + 0.5).toFixed(2))}))
+  console.log(items);
   let navigate = useNavigate();
 
-  const handleBuy = (event) => {
+  const handleBuy = async (event, itemprice, deliveryfee) => {
     console.log(event.target.value);
-    //make order here. 
-    let response = true;
-    if(response){
-      console.log(response)
-      navigate('/result',{
-        state:{
-          msg:`You order has been successful! View`, 
-          link:'orders'
-        }
-      });
-    }
+    console.log(itemprice);
+    console.log(deliveryfee);
 
+    let orderDetails = {
+      total: itemprice + deliveryfee, 
+      itemprice: itemprice,
+      deliveryfee: deliveryfee,
+      orderdate: getToday(), 
+      buyerid: user.userid,
+      itemid: event.target.value
+    }
+    console.log(orderDetails);
+    try{
+      let url = `${config["LOCALS_API"]}/orders`
+      let response = await axios.post(url, orderDetails);
+      if(response){
+        navigate('/result',{
+          state:{
+            msg:`You order has been successful! View`, 
+            link:'orders'
+          }
+        });
+      }
+    }catch(e){
+      console.error(e);
+    }
   }
 
   return(
@@ -51,7 +84,7 @@ const SearchItems = () => {
                 </Col>
                 <Col xs={3} className="my-auto">
                   <div className="text-center">
-                    <Button onClick={handleBuy} value={x.itemid}>Buy</Button>
+                    <Button onClick={(e) => handleBuy(e, x.price, x.deliveryfee)} value={x.itemid}>Buy</Button>
                   </div>
                 </Col>
               </Row>
--- Locals_Frontend/locals/src/components/Search.js
+++ Locals_Frontend/locals/src/components/Search.js
@@ -37,7 +37,7 @@ const Search = () => {
       state:{
         items:response.data,
         query: searchString,
-        suburbs: selectedSuburbs
+        suburbs: selectedSuburbs,
       }
     });
 
